ea3145fb863af67f71712326d9ed25dcd142d73d
Zhiyuan Liang <zhiyuanliang@microsoft.com>
2025-11-12 13:51:43 +0800

    add client example

---
+++ client-app/src/App.jsx
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ContextProvider } from "./pages/AppContext";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+
+function App() {
+  return (
+    <ContextProvider>
+      <Router>
+      <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </ContextProvider>
+  );
+}
+
+export default App;
