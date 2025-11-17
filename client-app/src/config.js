bcc8565f6f008f5d0e038f2316e0da3d0adbb34a
Zhiyuan Liang <zhiyuanliang@microsoft.com>
2025-11-17 14:09:52 +0800

    update

--- client-app/src/config.js
+++ client-app/src/config.js
@@ -1,4 +1,4 @@
-export const appConfigSettings = {
+export const config = {
   // Application Insights connection string (Instrumentation for telemetry)
   appInsightsConnectionString: "YOUR-APP-INSIGHTS-CONNECTION-STRING",
   // Azure Front Door endpoint hosting App Configuration data (feature flags, etc.)
